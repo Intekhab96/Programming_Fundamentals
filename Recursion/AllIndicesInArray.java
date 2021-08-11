96d34e7c58075882acc89c8c0f93fdf357f63f30
Mohd. Intekhab <mohdintekhab96@gmail.com>
2021-08-11 20:38:21 +0530

    practicing basics

---
+++ Recursion/AllIndicesInArray.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class AllIndicesInArray {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int x = Integer.parseInt(br.readLine());
+        int[] iarr = allIndices(arr, x, 0, 0);
+
+        if (iarr.length == 0) {
+            System.out.println();
+            return;
+        }
+
+        for (int i = 0; i < iarr.length; i++) {
+            System.out.println(iarr[i]);
+        }
+    }
+
+    public static int[] allIndices(int[] arr, int x, int idx, int fsf) {
+        if (idx == arr.length)
+            return new int[fsf];
+        int[] temp;
+        if (arr[idx] == x) {
+            temp = allIndices(arr, x, idx + 1, fsf + 1);
+            temp[fsf] = idx;
+        } else {
+            temp = allIndices(arr, x, idx + 1, fsf);
+        }
+
+        return temp;
+    }
+}
